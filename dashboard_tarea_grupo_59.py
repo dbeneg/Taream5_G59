69a27ca62a615e83652f26b32976d35f43fa8212
dbeneg <dbg.clases@gmail.com>
2025-05-18 14:43:41 -0400

    Update dashboard_tarea_grupo_59.py

--- dashboard_tarea_grupo_59.py
+++ dashboard_tarea_grupo_59.py
@@ -129,13 +129,12 @@ with col5:
     total_spend = df_filtrado.groupby("Customer type")["Total"].sum().reset_index()
     fig5, ax5 = plt.subplots(figsize=(6, 4))
 
-    custom_colors = ["#FF5733", "#33C1FF"] 
     
     sns.barplot(
         data=total_spend,
         x="Customer type",
         y="Total",
-        palette=custom_colors,
+        palette="Set1",
         ax=ax5
     )
     ax5.set_title("Gasto Total por Tipo de Cliente", loc="center")
